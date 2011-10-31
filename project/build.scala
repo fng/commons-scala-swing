583097b39c383bdc03a094cc541a83f88a5531d6
FNG <daniel.wydler@gmx.ch>
2011-10-31 21:46:31 +0100

    publish to local directory

--- project/build.scala
+++ project/build.scala
@@ -2,19 +2,25 @@ import sbt._
 import Keys._
 
 object build extends Build {
-	
-	val commonsWicketScala = Project(
-		id = "commons-scala-swing",
-		base = file("."),
-		settings = Defaults.defaultSettings ++ Seq[Project.Setting[_]](
-	      organization := "com.github.fng",
-	      version := "1.0.0-SNAPSHOT",
-	      scalaVersion := "2.9.1",
-	      crossPaths := false
-	    ) ++ Seq(
-		      libraryDependencies ++= Seq(ScalaSwing)
-		    )
-	)
-	
-	lazy val ScalaSwing = "org.scala-lang" % "scala-swing" % "2.9.1" withSources
+
+  val commonsWicketScala = Project(
+    id = "commons-scala-swing",
+    base = file("."),
+    settings = Defaults.defaultSettings ++ Seq[Project.Setting[_]](
+      organization := "com.github.fng",
+      version := "1.0.0-SNAPSHOT",
+      scalaVersion := "2.9.1",
+      crossPaths := false,
+      publishTo <<= (version) {
+        (v: String) =>
+          val repoSuffix = if (v.contains("-SNAPSHOT")) "snapshots" else "releases"
+          val resolver = Resolver.file("gh-pages", new File("/Users/daniel/code/fng.github.com/repo", repoSuffix))
+          Some(resolver)
+      }
+    ) ++ Seq(
+      libraryDependencies ++= Seq(ScalaSwing)
+    )
+  )
+
+  lazy val ScalaSwing = "org.scala-lang" % "scala-swing" % "2.9.1" withSources
 }
